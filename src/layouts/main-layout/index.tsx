a95f1de8732b90e6830cb778859bad2a13ab3f93
RafiKK98 <rafikk1998@gmail.com>
2024-07-16 10:31:38 +0600

    added a footer section with themewagon link

--- src/layouts/main-layout/index.tsx
+++ src/layouts/main-layout/index.tsx
@@ -2,6 +2,7 @@ import { useState, ReactElement, PropsWithChildren } from 'react';
 import { Box, Drawer, Toolbar } from '@mui/material';
 import Topbar from './Topbar/Topbar';
 import Sidebar from './Sidebar/Sidebar';
+import Footer from './Footer/Footer';
 // import { useBreakpoints } from 'providers/BreakpointsProvider';
 
 export const drawerOpenWidth = 240;
@@ -21,58 +22,61 @@ const MainLayout = ({ children }: PropsWithChildren): ReactElement => {
   // }, [isMobileScreen]);
 
   return (
-    <Box sx={{ display: 'flex', minHeight: '100vh' }}>
-      <Topbar open={open} handleDrawerToggle={handleDrawerToggle} />
-      {/* Mobile Drawer */}
-      <Drawer
-        variant="temporary"
-        open={open}
-        onClose={handleDrawerToggle}
-        ModalProps={{
-          keepMounted: true,
-        }}
-        sx={{
-          display: { xs: 'block', sm: 'none' },
-          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerOpenWidth },
-        }}
-      >
-        <Sidebar open={open} />
-      </Drawer>
-      {/* Desktop Drawer */}
-      <Drawer
-        variant="permanent"
-        component="aside"
-        open={open}
-        sx={{
-          display: { xs: 'none', sm: 'block' },
-          width: open ? drawerOpenWidth : drawerCloseWidth,
-          '& .MuiDrawer-paper': {
-            width: open ? drawerOpenWidth : drawerCloseWidth,
-          },
-        }}
-      >
-        <Sidebar open={open} />
-      </Drawer>
-      <Box
-        component="main"
-        overflow="auto"
-        sx={{
-          width: 1,
-          flexGrow: 1,
-          pt: 5,
-          pr: { xs: 3, sm: 5.175 },
-          pb: 6.25,
-          pl: { xs: 3, sm: 5.25 },
-        }}
-      >
-        <Toolbar
-          sx={{
-            height: 96,
+    <>
+      <Box sx={{ display: 'flex', minHeight: '100vh' }}>
+        <Topbar open={open} handleDrawerToggle={handleDrawerToggle} />
+        {/* Mobile Drawer */}
+        <Drawer
+          variant="temporary"
+          open={open}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
           }}
-        />
-        {children}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerOpenWidth },
+          }}
+        >
+          <Sidebar open={open} />
+        </Drawer>
+        {/* Desktop Drawer */}
+        <Drawer
+          variant="permanent"
+          component="aside"
+          open={open}
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            width: open ? drawerOpenWidth : drawerCloseWidth,
+            '& .MuiDrawer-paper': {
+              width: open ? drawerOpenWidth : drawerCloseWidth,
+            },
+          }}
+        >
+          <Sidebar open={open} />
+        </Drawer>
+        <Box
+          component="main"
+          overflow="auto"
+          sx={{
+            width: 1,
+            flexGrow: 1,
+            pt: 5,
+            pr: { xs: 3, sm: 5.175 },
+            pb: 6.25,
+            pl: { xs: 3, sm: 5.25 },
+          }}
+        >
+          <Toolbar
+            sx={{
+              height: 96,
+            }}
+          />
+          {children}
+        </Box>
       </Box>
-    </Box>
+      <Footer open={open} />
+    </>
   );
 };
 
